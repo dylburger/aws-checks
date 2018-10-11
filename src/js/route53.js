8905f396030418b13563dd6d441a8cf985cd6116
Dylan Sather <dylan.sather@gmail.com>
2018-10-10 23:38:38 -0700

    Adding config object.
    Using async / await syntax

--- src/js/route53.js
+++ src/js/route53.js
@@ -1,25 +1,20 @@
+import config from "./config";
 import AWS from "aws-sdk";
 
-const credentials = new AWS.SharedIniFileCredentials({ profile: "personal" });
+const credentials = new AWS.SharedIniFileCredentials(config);
 AWS.config.credentials = credentials;
 
 const route53 = new AWS.Route53();
 
-export function doesHostedZoneExist(domain) {
+export const doesHostedZoneExist = async domain => {
   const params = {
     DNSName: domain
   };
 
-  return new Promise((resolve, reject) => {
-    route53
-      .listHostedZonesByName(params)
-      .promise()
-      .then((data, err) => {
-        if (err) {
-          reject(err);
-        }
-
-        resolve(data.HostedZones[0].Name === domain);
-      });
-  });
-}
+  try {
+    const data = await route53.listHostedZonesByName(params).promise();
+    return data.HostedZones[0].Name === domain;
+  } catch (err) {
+    return false;
+  }
+};
